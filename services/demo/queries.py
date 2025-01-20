7774c98639206604ab3e1798bbdaa4dec7a317df
MalcomChumchouaPenda <malcom.chumchouapenda@yahoo.fr>
2025-01-20 18:42:04 +0100

    Debug events sampling

--- services/demo/queries.py
+++ services/demo/queries.py
@@ -1,6 +1,4 @@
 
-import os
-# import pandas as pd
 from core.utils import get_store
 from . import schemas as sch
 
@@ -58,57 +56,10 @@ def get_courses(session, formation=None, unit=None):
     if formation:
         query = query.filter(sch.Option.formation_id==formation)
     if unit:
-        query = query.filter_by(sch.Option.unit_id==unit)
+        query = query.filter(sch.Option.unit_id==unit)
     return query.all()
-    return [course for course, _ in query.all()]
-    courses = store.read_json('json/courses.json')
-    diplomas = store.read_json('json/diplomas.json')
-    diplomas = {d['id']:d for d in diplomas}
-    levels = store.read_json('json/levels.json')
-    levels = {l['id']:l for l in levels}
-    options = get_options(formation=formation, unit=unit)
-    options = {o['id']:o for o in options}
-    filters = list(options.keys())
-    results = []
-    for course in courses:
-        option_id = course['option_id']
-        if  option_id in filters:
-            option = options[option_id]
-            diploma_id = course['diploma_id']
-            diploma = diplomas[diploma_id]
-            level_id = course['level_id']
-            level = levels[level_id]
-            course['level_name'] = level['nom']
-            course['diploma_name'] = diploma['nom']
-            course['formation_id'] = option['formation_id']
-            course.update(options[course['option_id']])
-            course['id'] = f"{option_id}-{level_id}"
-            results.append(course)
-    return results
 
 def get_course(session, key):
     query = session.query(sch.Course)
     query = query.filter_by(id=key)
     return query.one()
-    diplomas = store.read_json('json/diplomas.json')
-    diplomas = {d['id']:d for d in diplomas}
-    levels = store.read_json('json/levels.json')
-    levels = {l['id']:l for l in levels}
-    options = store.read_json('json/options.json')
-    options = {o['id']:o for o in options}
-    courses = store.read_json('json/courses.json')
-    for course in courses:
-        option_id = course['option_id']
-        level_id = course['level_id']
-        course_id = f"{option_id}-{level_id}"
-        if key == course_id:
-            option = options[option_id]
-            diploma_id = course['diploma_id']
-            diploma = diplomas[diploma_id]
-            level = levels[level_id]
-            course['level_name'] = level['nom']
-            course['diploma_name'] = diploma['nom']
-            course['formation_id'] = option['formation_id']
-            course.update(options[course['option_id']])
-            course['id'] = course_id
-            return course
--- services/demo/routes.py
+++ services/demo/routes.py
@@ -14,130 +14,95 @@ TIME_EXP = r'%d/%m/%Y'
 @api.before_app_first_request
 def init_db():
     session = db.session
-    categories = init_categories(session)
-    _ = init_events(session, categories)
-    _ = init_pays(session)
-    _ = init_professions(session)
-    _ = init_formations(session)
-    _ = init_departments(session)
-    _ = init_labs(session)
-    _ = init_levels(session)
-    _ = init_diplomas(session)
-    _ = init_options(session)
-    _ = init_courses(session)
+    init_categories(session)
+    init_events(session)
+    init_pays(session)
+    init_professions(session)
+    init_formations(session)
+    init_departments(session)
+    init_labs(session)
+    init_levels(session)
+    init_diplomas(session)
+    init_options(session)
+    init_courses(session)
 
 def init_categories(session):
-    names = ['Formation', 'Organisation', 'Recherche']
-    result = {}
-    for name in names:
-        id_ = name[:3].upper()
-        category = sch.Category(id=id_, name=name)
-        result[name] = category
-        session.merge(category)
+    records = store.read_json('samples/event_categories.json')
+    for record in records:
+        obj = sch.Category(**record)
+        session.merge(obj)
     session.commit()
-    return result
 
-def init_events(session, categories):
+def init_events(session):
     records = store.read_json('samples/events.json')
-    result = []
     for record in records:
         record['date'] = dt.strptime(record['date'], TIME_EXP)
-        record['category'] = categories[record['category']]
         obj = sch.Event(**record)
         session.merge(obj)
-        result.append(obj)
     session.commit()
-    return result
 
 def init_pays(session):
     records = store.read_json('samples/pays.json')
-    result = []
     for record in records:
         obj = sch.Pays(**record)
         session.merge(obj)
-        result.append(obj)
     session.commit()
-    return result
 
 def init_professions(session):
     records = store.read_json('samples/professions.json')
-    result = []
     for record in records:
         obj = sch.Profession(**record)
         session.merge(obj)
-        result.append(obj)
     session.commit()
-    return result
 
 def init_formations(session):
     records = store.read_json('samples/formations.json')
-    result = []
     for record in records:
         obj = sch.Formation(**record)
         session.merge(obj)
-        result.append(obj)
     session.commit()
-    return result
 
 def init_departments(session):
     records = store.read_json('samples/departments.json')
-    result = []
     for record in records:
         obj = sch.Department(**record)
         session.merge(obj)
-        result.append(obj)
     session.commit()
-    return result
 
 def init_labs(session):
     records = store.read_json('samples/labs.json')
-    result = []
     for record in records:
         obj = sch.Lab(**record)
         session.merge(obj)
-        result.append(obj)
     session.commit()
-    return result
 
 def init_levels(session):
     records = store.read_json('samples/levels.json')
-    result = []
     for record in records:
         obj = sch.Level(**record)
         session.merge(obj)
-        result.append(obj)
     session.commit()
-    return result
 
 def init_diplomas(session):
     records = store.read_json('samples/diplomas.json')
-    result = []
     for record in records:
         obj = sch.Diploma(**record)
         session.merge(obj)
-        result.append(obj)
     session.commit()
-    return result
 
 def init_options(session):
     records = store.read_json('samples/options.json')
-    result = []
     for record in records:
         obj = sch.Option(**record)
         session.merge(obj)
-        result.append(obj)
     session.commit()
-    return result
 
 def init_courses(session):
     records = store.read_json('samples/courses.json')
-    result = []
     for record in records:
         obj = sch.Course(**record)
         session.merge(obj)
-        result.append(obj)
     session.commit()
-    return result
 
 
 @api.route('/')
