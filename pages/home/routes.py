8f2aa20cd2d94b854baadd6e851a2a5a11df627f
MalcomChumchouaPenda <malcom.chumchouapenda@yahoo.fr>
2025-01-20 20:46:39 +0100

    Simplify home routing

--- pages/home/routes.py
+++ pages/home/routes.py
@@ -12,43 +12,23 @@ event_assets = get_assets('events')
 
 @ui.route('/')
 def index():
-    about = _load_about()
-    heros = _load_heros()
-    stats = _load_stats()
-    speech = _load_speech()
-    events = _load_events()
-    features = _load_features()
-    return render_template('home.html', 
-                           about=about,
-                           heros=heros,
-                           stats=stats,
-                           speech=speech,
-                           events=events,
-                           features=features)
-
-def _load_heros():
+    speech = '/home/assets/md/speech.md'
+    left = '/home/assets/md/about-left.md'
+    right = '/home/assets/md/about-right.md'
+    about = dict(left=left, right=right)
     heros = []
     for i in range(3):
-        msg = assets.read_markdown(f'md/hero-msg-{i+1}.md')
-        img = f'img/hero-bg-{i+1}.jpg'
+        msg = f'/home/assets/md/hero-msg-{i+1}.md'
+        img = f'/home/assets/img/hero-bg-{i+1}.jpg'
         heros.append(dict(msg=msg, img=img))
-    return heros
-
-def _load_speech():
-    return assets.read_markdown(f'md/speech.md')
-
-def _load_about():
-    left = assets.read_markdown(f'md/about-left.md')
-    right = assets.read_markdown(f'md/about-right.md')
-    return dict(left=left, right=right)
-
-def _load_stats():
-    return assets.read_json('json/stats.json')
-
-def _load_features():
-    return assets.read_json(f'json/features.json')
-
-def _load_events():
-    recent_events = qry.get_events(db.session, level=0)
-    return recent_events
+    events = qry.get_events(db.session, level=0)
+    features = assets.read_json(f'json/features.json')
+    stats = assets.read_json('json/stats.json')
+    return render_template('home.html', 
+                           about=about,
+                           speech=speech,
+                           heros=heros,
+                           stats=stats,
+                           events=events,
+                           features=features)
 
