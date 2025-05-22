4561d1289f7ab651f7cbf7e09ae8240c71450f2a
MalcomChumchouaPenda <malcom.chumchouapenda@yahoo.fr>
2025-05-22 02:36:33 +0100

    structure dashboard demos

--- plugins/pages/demo/routes.py
+++ plugins/pages/demo/routes.py
@@ -134,6 +134,7 @@ def events():
 
 
 @ui.route('/dashboard')
+@ui.login_required
 def dashboard():
     welcome = _("Cette espace presente des composants demo")
     return render_template('dashboard/home.jinja',
@@ -142,42 +143,49 @@ def dashboard():
 
 
 @ui.route('/charts')
+@ui.roles_accepted('developper')
 def charts():
     return render_template('demo-charts.jinja')
 
 @ui.route('/tables')
+@ui.roles_accepted('developper')
 def tables():
     persons = [
-        {'name':'Brandon Jacob', 'positions':'Designer', 'age':28, 'start_date':'2016-05-25'},
-        {'name':'Bridie Kessler', 'positions':'Developper', 'age':35, 'start_date':'2014-12-05'},
-        {'name':'Ashleigh Langosh', 'positions':'Finance', 'age':45, 'start_date':'2011-08-12'},
-        {'name':'Angus Grady', 'positions':'HR', 'age':34, 'start_date':'2012-06-11'},
-        {'name':'Raheem Lehner', 'positions':'Designer', 'age':47, 'start_date':'2011-04-19'},
+        {'name':'Brandon Jacob', 'position':'Designer', 'age':28, 'start_date':'2016-05-25'},
+        {'name':'Bridie Kessler', 'position':'Developper', 'age':35, 'start_date':'2014-12-05'},
+        {'name':'Ashleigh Langosh', 'position':'Finance', 'age':45, 'start_date':'2011-08-12'},
+        {'name':'Angus Grady', 'position':'HR', 'age':34, 'start_date':'2012-06-11'},
+        {'name':'Raheem Lehner', 'position':'Designer', 'age':47, 'start_date':'2011-04-19'},
     ]
     return render_template('demo-tables.jinja', persons=persons)
 
 @ui.route('/datatables')
+@ui.roles_accepted('developper')
 def datatables():
     addrspath = os.path.join(static_dir, 'json/addresses.json')
     return render_template('demo-datatables.jinja', addresses=read_json(addrspath))
 
 
 @ui.route('/form-editors')
+@ui.roles_accepted('developper')
 def form_editors():
     return render_template('demo-form-editors.jinja')
 
 
 @ui.route('/form-elements')
+@ui.roles_accepted('developper')
 def form_elements():
     return render_template('demo-form-elements.jinja')
 
 
 @ui.route('/form-layouts')
+@ui.roles_accepted('developper')
 def form_layouts():
     return render_template('demo-form-layouts.jinja')
 
 
 @ui.route('/form-validation')
+@ui.roles_accepted('developper')
 def form_validation():
     return render_template('demo-form-validation.jinja')
 
