19c62de6e921c4fac464014c6540d100a7eb9114
MalcomChumchouaPenda <malcom.chumchouapenda@yahoo.fr>
2025-05-23 08:35:18 +0100

    separate landing and dashboard demos

---
+++ plugins/pages/demo_dashboard/routes.py
@@ -0,0 +1,93 @@
+
+import os
+from flask_babel import gettext as _
+from flask_babel import lazy_gettext as _l
+from flask import render_template, request, url_for, redirect
+from core.utils import (
+    UiBlueprint, 
+    read_json, 
+    read_markdown, 
+    paginate_items, 
+    default_deadline,
+    get_locale
+)
+
+
+ui = UiBlueprint(__name__)
+static_dir = os.path.join(os.path.dirname(__file__), 'static')
+
+
+@ui.route('/dashboard')
+@ui.login_required
+def dashboard():
+    welcome = _("Cette espace presente des composants demo")
+    return render_template('dashboard/home.jinja',
+                           domain_ids=['g1', 'g2'],  
+                           welcome=welcome)
+
+
+@ui.route('/charts')
+@ui.roles_accepted('developper')
+def charts():
+    return render_template('demo-charts.jinja')
+
+@ui.route('/tables')
+@ui.roles_accepted('developper')
+def tables():
+    persons = [
+        {'name':'Brandon Jacob', 'position':'Designer', 'age':28, 'start_date':'2016-05-25'},
+        {'name':'Bridie Kessler', 'position':'Developper', 'age':35, 'start_date':'2014-12-05'},
+        {'name':'Ashleigh Langosh', 'position':'Finance', 'age':45, 'start_date':'2011-08-12'},
+        {'name':'Angus Grady', 'position':'HR', 'age':34, 'start_date':'2012-06-11'},
+        {'name':'Raheem Lehner', 'position':'Designer', 'age':47, 'start_date':'2011-04-19'},
+    ]
+    return render_template('demo-tables.jinja', persons=persons)
+
+@ui.route('/datatables')
+@ui.roles_accepted('developper')
+def datatables():
+    addrspath = os.path.join(static_dir, 'json/addresses.json')
+    return render_template('demo-datatables.jinja', addresses=read_json(addrspath))
+
+
+@ui.route('/form-editors')
+@ui.roles_accepted('developper')
+def form_editors():
+    return render_template('demo-form-editors.jinja')
+
+
+@ui.route('/form-elements')
+@ui.roles_accepted('developper')
+def form_elements():
+    return render_template('demo-form-elements.jinja')
+
+
+@ui.route('/form-layouts')
+@ui.roles_accepted('developper')
+def form_layouts():
+    return render_template('demo-form-layouts.jinja')
+
+
+@ui.route('/form-validation')
+@ui.roles_accepted('developper')
+def form_validation():
+    return render_template('demo-form-validation.jinja')
+
+
+# @ui.route('/unprotected')
+# def unprotected():
+#     message="Welcome in a free space"
+#     return render_template('home-space.jinja', message=message)
+
+@ui.route('/protected')
+@ui.login_required
+def protected():
+    message = _("Welcome in a protected space")
+    return render_template('dashboard/home.jinja',welcome=message)
+
+@ui.route('/student')
+@ui.roles_accepted('student')
+def student():
+    message = _("Welcome in student space")
+    return render_template('dashboard/home.jinja',welcome=message)
+
